d2cc807b6770e24d2f9f4ac859a9c003a9debc9d
JathinB <battepatijathin@gmail.com>
2026-08-23 13:42:51 -0500

    Changes

--- Flutter APp/seatbelt_detector_app/android/app/build.gradle.kts
+++ Flutter APp/seatbelt_detector_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.seatbelt_detector_app"
+    namespace = "com.beltsense.app"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.seatbelt_detector_app"
+        applicationId = "com.beltsense.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- Flutter APp/seatbelt_detector_app/android/app/src/main/kotlin/com/beltsense/app/MainActivity.kt
+++ Flutter APp/seatbelt_detector_app/android/app/src/main/kotlin/com/beltsense/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.seatbelt_detector_app
+package com.beltsense.app
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
